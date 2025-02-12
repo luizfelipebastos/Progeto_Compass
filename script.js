5c3af5012e7d101ba7a6ee3fd68bba1e9b44e1a3
Luiz <contato.felipebastos@gmail.com>
2025-02-12 18:18:09 -0300

    att home-page

--- script.js
+++
@@ -1,6 +0,0 @@
-const fecharPopupButton = document.querySelector(".fechar-popup");
-const popup = document.querySelector(".popup");
-
-fecharPopupButton.addEventListener("click", () => {
-  popup.style.display = "none";
-});
